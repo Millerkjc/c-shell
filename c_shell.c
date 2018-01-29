3cf1d54fc868726bbee557c1f98f939a9ad33dc8
Mill <millerkjc@gmail.com>
2018-01-29 14:02:39 +0100

    separation functions-main [shell_utility - free,read,split, etc]

--- c_shell.c
+++ c_shell.c
@@ -8,9 +8,9 @@
 
 //My library
 #include "builtin.h"
+#include "shell_utility.h"
 
 #define LEN 80
-#define MAX_ARG 10
 #define LEN_ARG 10
 #define N_FUNC 2
 
@@ -19,14 +19,8 @@ typedef struct{
 	void (*f_builtin)();
 }func_dict;
 
-char *read_line(char *buf, size_t sz);
-void split(char *buf, char *split[], size_t max);
 int builtin_f(func_dict func_d[], char *str);
 void exit_builtin(char **arr);
-//void cd_builtin(char **arr);
-//void cd_builtin(char *path);
-int background(char **arr);
-void free_arr(char **arr);
 
 int main(){
 	char str[LEN];
@@ -132,33 +126,6 @@ int main(){
 	return 0;
 }
 
-char *read_line(char *buf, size_t sz){
-	char *curDir = getcwd(NULL,0);
-	if(!curDir){
-		fprintf(stderr, "getcwd: %s\n", strerror(errno));
-		exit(1);
-	}
-	//printf("%s> ", curDir);
-	printf("Too> ");
-	free(curDir);
-	return fgets(buf, sz, stdin);
-}
-
-void split(char *buf, char *split[], size_t max){
-	const char *s = "& \n";
-	size_t i=0;
-
-	buf = strtok(buf,s);
-
-	while(buf != NULL && i<max-1) {
-		*(buf + strlen(buf)) = '\0';
-		strcpy(split[i], buf);
-		i+=1;
-		buf = strtok(NULL, s);
-	}
-	split[i]=NULL;
-}
-
 int builtin_f(func_dict func_d[], char *str){
 	int i;
 	for(i=0;i<N_FUNC;i++){
@@ -177,37 +144,4 @@ void exit_builtin(char **arr){
 	free(arr);
 	printf("Exiting..\n");
 	exit(0);
-}
-
-//void cd_builtin(char *path){
-/*
-void cd_builtin(char **arr){
-	char *path = arr[1];
-	int ret =chdir(path);
-	if(ret==-1){
-		fprintf(stderr, "%s: %s\n", path, strerror(errno));
-	}
-}
-*/
-
-int background(char **arr){
-	int i;
-	for(i=1;arr[i]!=NULL && i<MAX_ARG;i++){
-		if(strstr(arr[i],"&")!=NULL){
-			arr[i][strlen(arr[i])-1]='\0';
-			return i;
-		}else if(!strcmp(arr[i],"&")){
-			arr[i] = NULL;
-			return i;
-		}
-	}
-	return -1;
-}
-
-void free_arr(char **arr){
-	int i;
-	for(i=0;i<MAX_ARG;i++){
-		free(arr[i]);
-	}
-	free(arr);
 }
